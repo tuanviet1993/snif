d8a37fb11f93973a40c4f4b4da2b8d50c8f70048
matthias.ringwald <matthias.ringwald@1d862dab-282a-0410-9ca0-21c2332eed07>
2007-03-20 13:17:27 +0000

    added registerGroup (K gID, long timestamp) to generate empty aggregates after at most timewindow

--- trunk/src/stream/TimeWindowGroupAggregator.java
+++ trunk/src/stream/TimeWindowGroupAggregator.java
@@ -1,5 +1,6 @@
 package stream;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class TimeWindowGroupAggregator<I, K, O> extends AbstractPipe<I, O> implements TimeTriggered {
@@ -8,7 +9,9 @@ public class TimeWindowGroupAggregator<I, K, O> extends AbstractPipe<I, O> imple
 	protected Function<I,? extends K> grouper;
 	protected GroupAggregationFunction<I,K,O> aggregator;
 	protected LinkedList<TimeStampedObject<I>> window = new LinkedList<TimeStampedObject<I>>();
-	
+	protected LinkedList<TimeStampedObject<K>> initList = new LinkedList<TimeStampedObject<K>>();
+	protected HashMap<K, Object> registeredGroups = new HashMap<K, Object>();
+
 	public void handleTimerEvent(long timestamp) {
 		// check entries for removal
 		while (window.size() > 0 && window.getFirst().timestamp + timewindow <= timestamp) {
@@ -16,13 +19,37 @@ public class TimeWindowGroupAggregator<I, K, O> extends AbstractPipe<I, O> imple
 			window.removeFirst();
 			aggregateOverList(timestamp, gId);
 		}
+		// handle init timeouts for registered groupds
+		while (initList.size()>0 && initList.getFirst().timestamp + timewindow <= timestamp) {
+			K gID = initList.removeFirst().object;
+			aggregateOverList( timestamp, gID);
+		}
 	}
 	
+	/**
+	 * Register group explicitly with operator to receive an aggregate even if no other items are processed
+	 * @param gID
+	 * @param timestamp
+	 */
+	public void registerGroup(K gID, long timestamp ) {
+		if (!registeredGroups.containsKey(gID)) {
+			registeredGroups.put(gID, null);
+			initList.addLast(new TimeStampedObject<K>(timestamp, gID));
+			Scheduler.getInstance().registerTimeout(timestamp + timewindow,
+					this);
+		}
+	}
+
 	public void process(I o, int srcID, long timestamp) {
 		// add to list
 		window.addLast(new TimeStampedObject<I>(timestamp,o));
 		// get group id
 		K gID = grouper.invoke( o );
+		// assert group is registered
+		if (!registeredGroups.containsKey(gID)) {
+			registeredGroups.put( gID, null);
+		}
+		// aggregate
 		aggregateOverList(timestamp, gID);
 		// register timeout
 		Scheduler.getInstance().registerTimeout( timestamp + timewindow, this );
